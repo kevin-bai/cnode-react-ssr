1dfa9e27793da31dd2e82cdfdfbd758a1b9f8c0c
Kevin J Bai <897840029@qq.com>
2019-05-14 13:33:36 +0800

    fix : 修复 ssr 中， favicon无法显示问题

--- server/server.js
+++ server/server.js
@@ -1,33 +1,35 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
+const favicon = require('serve-favicon')
 const ReactSSR = require('react-dom/server')
 
 const app = express()
 
 const isDev = process.env.NODE_ENV === 'development'
+app.use(favicon(path.join(__dirname, '../favicon.ico')))
 
-if (!isDev){
-    const serverEntry = require('../dist/server-entry.js').default  // 因为导出的时候是用es6的export default
+if (!isDev) {
+  const serverEntry = require('../dist/server-entry.js').default  // 因为导出的时候是用es6的export default
 
-    // 因为app.get('*'),要刨除静态文件不走服务端渲染
-    app.use('/public',express.static(path.join(__dirname, '../dist')))
+  // 因为app.get('*'),要刨除静态文件不走服务端渲染
+  app.use('/public', express.static(path.join(__dirname, '../dist')))
 
-    //静态文件不需要走ssr
-    const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8')
+  //静态文件不需要走ssr
+  const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8')
 
-    app.get('*', (req, res) => {
-        const appString = ReactSSR.renderToString(serverEntry);
-        const str = template.replace('<!-- app -->', appString)
-        res.send(str)
-    })
+  app.get('*', (req, res) => {
+    const appString = ReactSSR.renderToString(serverEntry);
+    const str = template.replace('<!-- app -->', appString)
+    res.send(str)
+  })
 
-}else {
-    const devStatic = require('./util/dev-static')
-    devStatic(app)
+} else {
+  const devStatic = require('./util/dev-static')
+  devStatic(app)
 
 }
 
 app.listen(3333, function () {
-    console.log('server is listening on port 3333')
+  console.log('server is listening on port 3333')
 })
